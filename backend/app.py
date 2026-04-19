d031cb5dce85859c9a6a90b37c079bf475290bea
PolarNEWCODER <rafael.end93@gmail.com>
2026-04-19 15:46:23 -0300

    Atualizando projeto completo

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"msg": "API funcionando"})
+    return jsonify({"msg": "API rodando"})
 
-app.run(debug=True) 
+@app.route('/produto', methods=['POST'])
+def produto():
+    dados = request.json
+    return jsonify({"msg": "Produto cadastrado", "dados": dados})
+
+app.run(debug=True)
