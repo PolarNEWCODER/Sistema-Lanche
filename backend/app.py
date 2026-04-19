2759a67a229839c1091de9309a3f2a484cd7bbd3
PolarNEWCODER <rafael.end93@gmail.com>
2026-04-19 16:34:11 -0300

    frontend com listagem funcionando

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,35 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
+# "Banco fake" em memória
+produtos = []
+vendas = []
+
+# TESTE
 @app.route('/')
 def home():
     return jsonify({"msg": "API rodando"})
 
+# CADASTRAR PRODUTO
 @app.route('/produto', methods=['POST'])
-def produto():
+def cadastrar_produto():
     dados = request.json
-    return jsonify({"msg": "Produto cadastrado", "dados": dados})
+    produtos.append(dados)
+    return jsonify({"msg": "Produto cadastrado", "produtos": produtos})
+
+# LISTAR PRODUTOS
+@app.route('/produtos', methods=['GET'])
+def listar_produtos():
+    return jsonify(produtos)
+
+# REALIZAR VENDA
+@app.route('/venda', methods=['POST'])
+def venda():
+    dados = request.json
+    vendas.append(dados)
+    return jsonify({"msg": "Venda realizada", "vendas": vendas})
 
 app.run(debug=True)
